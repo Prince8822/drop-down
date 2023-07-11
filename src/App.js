93ff35e79e20d3c7bb4ab8f246d635d22a0ffa2a
unknown <prince_00@hotmail.com>
2023-07-11 07:55:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Should you use a dropdown?</h1>
+  <ul class="drop">
+  
+    <a href="" class="btn">Select  </a>
+    <div class="drop-Menu">
+      <a href="">yes</a>
+      <a href="">Probably not</a>
+    </div>
+  </ul>
+
     </div>
   );
 }
 
+
 export default App;
+
+
